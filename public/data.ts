f160f4ba96562e329a3d44aca8e3a4fc61b7fa35
MohamedAbdelaziz19 <davidpunisher475@gmail.com>
2024-10-14 22:43:22 +0100

    Last Update For Deploy b

--- public/data.ts
+++ public/data.ts
@@ -1,15 +1,15 @@
-import { CldImage } from "next-cloudinary";
+
 const projects = [
     {
       title: 'Web Site',
-      imgSrc: 'https://res.cloudinary.com/dzo2bvw5a/image/upload/v1724568562/aziz-portfolio/erz04qlpa35n3rghqqlw.png',
+      imgSrc: 'https://res.cloudinary.com/duhfghkqq/image/upload/v1728932579/img/aplkfc0iecgtdavxuc7d.png',
       description: 'Click here to visit',
       link: 'https://github.com/MohamedAbdelaziz19',
     },
   
     {
       title: 'Filmmaker',
-      imgSrc: 'https://res.cloudinary.com/dzo2bvw5a/image/upload/v1724568552/aziz-portfolio/nffaswbva05u5v0jg3dv.png',
+      imgSrc: 'https://res.cloudinary.com/duhfghkqq/image/upload/v1728932577/img/rfdtzfomhrwzldak4mlw.png',
       description: 'Click here to visit',
       link: 'https://www.behance.net/azizomohamed',
     },
